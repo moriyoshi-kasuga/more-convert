f8d839a53a1a180d602c11f9f0eb4cf15e4f5f18
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-01-26 08:32:39 +0900

    fix: fix crate doc path

--- more-convert/src/lib.rs
+++ more-convert/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!(concat!("../", std::env!("CARGO_PKG_README")))]
 
 pub use more_convert_derive::Convert;
 pub use more_convert_derive::EnumRepr;
